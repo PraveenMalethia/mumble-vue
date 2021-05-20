8901c9aac94dd35eef555c69d87877c2e03631fb
ParveenMalethia <prince.malethia.36@gmail.com>
2021-05-20 10:08:21 +0530

    setup

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,23 +1,21 @@
 <template>
   <div>
+    <!-- Nav Starts -->
     <nav>
-      <div class="mumble-logo">
-        Mumble
-      </div>
-      <div class="search box">
-        Search
-      </div>
-      <div class="Notification icon">
-        Notification
-      </div>
-      <div class="profile">
-        Profile Avatar
-      </div>
+      <div class="mumble-logo">Mumble</div>
+      <div class="search-box">Search</div>
+      <div class="notification-icon">Notification</div>
+      <div class="profile-avatar">Profile Avatar</div>
     </nav>
+    <!-- Nav Ends -->
+    <!-- Main App starts -->
     <Nuxt />
+    <!-- Main App ends -->
+    <!-- Footer starts -->
+    <div class="footer">Footer</div>
+    <!-- Footer ends -->
   </div>
 </template>
 
 <style>
-
 </style>
