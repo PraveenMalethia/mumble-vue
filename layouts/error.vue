dcce7d31b97ab72dde8c393df0a10ff3278d71d4
ParveenMalethia <prince.malethia.36@gmail.com>
2021-05-23 13:11:35 +0530

    Login and Error Created

---
+++ layouts/error.vue
@@ -0,0 +1,46 @@
+<template>
+  <div
+    class="min-w-screen min-h-screen bg-blue-100 flex items-center p-5 lg:p-20 overflow-hidden relative"
+  >
+    <div
+      class="flex-1 min-h-full min-w-full rounded-3xl bg-white shadow-xl p-10 lg:p-20 text-gray-800 relative md:flex items-center text-center md:text-left"
+    >
+      <div class="w-full md:w-1/2">
+        <div class="mb-10 md:mb-20 text-gray-600 font-light">
+          <h1
+            class="font-black uppercase text-3xl lg:text-5xl text-indigo-700 mb-10"
+          >
+            Error<br />404
+          </h1>
+          <p class="">Page not found</p>
+        </div>
+        <div class="mb-20 md:mb-0">
+          <button @click="$router.push('/')"
+            class="text-lg font-light outline-none focus:outline-none transform transition-all hover:scale-110 text-blue-500 hover:text-blue-600"
+          >
+            Back to Home
+          </button>
+        </div>
+      </div>
+      <div class="w-full md:w-1/2 text-center">
+        <img
+          src="https://flipstore.withun.link/images/404.png"
+          class="w-64 h-64"
+        />
+      </div>
+    </div>
+    <div
+      class="w-64 md:w-96 h-96 md:h-full bg-blue-200 bg-opacity-30 absolute -top-64 md:-top-96 right-20 md:right-32 rounded-full pointer-events-none -rotate-45 transform"
+    ></div>
+    <div
+      class="w-96 h-full bg-indigo-200 bg-opacity-20 absolute -bottom-96 right-64 rounded-full pointer-events-none -rotate-45 transform"
+    ></div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
