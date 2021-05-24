d016673b039f19dd216a2883ab0c0342e6c7af13
srikarkandula <79042534+srikarkandula@users.noreply.github.com>
2021-05-24 13:55:26 +0530

    Update tags.vue

--- components/tags.vue
+++ components/tags.vue
@@ -1,10 +1,13 @@
 <template>
-  
+  <button class="bg-gray-700 text-base border-2 py-1 px-3 rounded-md abolute right-5 mr-1 text-white 
+        text-sm font-semibold">{{text}}</button> 
 </template>
 
 <script>
 export default {
-
+    props:{
+        text:''
+    }
 }
 </script>
 
