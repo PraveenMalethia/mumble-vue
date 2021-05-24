cfbbe033efef98284deb5ce243a2f654a5b71646
ParveenMalethia <prince.malethia.36@gmail.com>
2021-05-25 01:58:16 +0530

    Description added

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'Mumble | Tech space for real developers' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
