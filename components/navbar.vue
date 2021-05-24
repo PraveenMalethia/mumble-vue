620576f24569b5c5f965a262ff0d2a2ecf67b829
ParveenMalethia <prince.malethia.36@gmail.com>
2021-05-25 02:13:30 +0530

    🔔 Notification is Now Button

--- components/navbar.vue
+++ components/navbar.vue
@@ -11,12 +11,12 @@
 
             <div>
                 <div class="flex items-center">
-                    <p class="text-white ml-5 uppercase md:text-lg md:tracking-medium md:mr-6" @click="show_items = !show_items"><fa icon="bell"/></p>
+                    <button class="text-white ml-5 uppercase md:text-lg md:tracking-medium md:mr-6" @click="show_items = !show_items"><fa class="cursor-finger" icon="bell"/></button>
 
                     <img src="https://mumbleapi.herokuapp.com/static/images/default.png" class="h-10 mr-4 md:mr-12 lg:mr-16">
                 </div>
                     
-                <!-- <p class="text-white ml-5 uppercase md:text-lg md:tracking-medium ml-4 md:mr-6" @click="show_items = !show_items">Bell</p>
+                <!-- <p class="text-white uppercase md:text-lg md:tracking-medium ml-4 md:mr-6" @click="show_items = !show_items">Bell</p>
 
                 <img src="https://mumbleapi.herokuapp.com/static/images/default.png" class="h-10 mr-4 md:mr-12 lg:mr-16"> -->
 
--- components/sidenavbar.vue
+++ components/sidenavbar.vue
@@ -1,30 +1,36 @@
 <template>
-    <div class="px-4 ml-24 mt-0 fixed hidden md:block">
-        <div v-for="tab in tabs" :key="tab.id">
-            <p class="cursor-pointer transition ease-in duration-300
-             rounded-full mr-auto font-medium text-lg tracking-wide inline-block p-3 mb-6"
-            ><span v-if="current == tab.id" class="text-blue-600" >{{ tab.title }}</span>
-            <span v-else class="text-gray-700"><fa class="mr-2" :icon="tab.icon"/>{{ tab.title }}</span></p>
-        </div>
+  <div class="px-4 ml-24 mt-0 fixed hidden md:block">
+    <div v-for="tab in tabs" :key="tab.id">
+      <p
+        class="cursor-pointer transition ease-in duration-300 rounded-full mr-auto font-medium text-lg tracking-wide inline-block p-3 mb-6"
+      >
+        <span v-if="current == tab.id" class="text-blue-600">{{
+          tab.title
+        }}</span>
+        <span v-else class="text-gray-700"
+          ><fa class="mr-2" :icon="tab.icon" />{{ tab.title }}</span
+        >
+      </p>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name:'SideNavBar',
-    data(){
-        return{
-            tabs: [
-                {title: 'Home', id:1,icon: 'home'},
-                {title: 'Contributers', id:2,icon: 'users'},
-                {title: 'Inbox', id: 3,icon: 'inbox'},
-                {title: 'Topics', id: 4,icon: 'tags'},
-                {title: 'Settings', id: 5,icon: 'cog'},
+  name: 'SideNavBar',
+  data() {
+    return {
+      tabs: [
+        { title: 'Home', id: 1, icon: 'home' },
+        { title: 'Contributers', id: 2, icon: 'users' },
+        { title: 'Inbox', id: 3, icon: 'inbox' },
+        { title: 'Topics', id: 4, icon: 'tags' },
+        { title: 'Settings', id: 5, icon: 'cog' },
       ],
-        }
-    },
-    props:{
-        current:0
-    },
+    }
+  },
+  props: {
+    current: 0,
+  },
 }
 </script>
