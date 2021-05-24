6572679657ab0f43f60f3018e4b71284b85a0924
ParveenMalethia <prince.malethia.36@gmail.com>
2021-05-24 22:42:14 +0530

    Width fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-gray-100 h-screen">
-    <div class="bg-gray-100 grid md:grid-cols-3 lg:grid-cols-4 max-w-7xl dark:bg-gray-800 py-4 px-4 sm:px-6 lg:py-4">
+    <div class="bg-gray-100 grid md:grid-cols-3 lg:grid-cols-4 m-auto dark:bg-gray-800 py-4 px-4 sm:px-6 lg:py-4">
       <div class="lg:col-span-1 hidden lg:block mt-10 md:mt-20">
         <sidenavbar current="home" />
       </div>
