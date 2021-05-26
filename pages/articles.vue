1baecb3136859c4b0de2cff357ca802e823e1bba
ParveenMalethia <prince.malethia.36@gmail.com>
2021-05-26 13:18:54 +0530

    🟢 Sidenav Working and Other pages added

---
+++ pages/articles.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="min-h-screen">
+    <h1>Welcome to articles Page</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
---
+++ pages/settings.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="min-h-screen">
+    <h1>Welcome to Settings Page</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
