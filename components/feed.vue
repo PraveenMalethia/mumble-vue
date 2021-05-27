bb15054cbb34ffc097be3754135a3bc11816b475
ParveenMalethia <prince.malethia.36@gmail.com>
2021-05-27 12:20:14 +0530

    Mumble can't be Empty

--- components/feed.vue
+++ components/feed.vue
@@ -9,7 +9,7 @@
         class="p-4 mt-4 bg-gray-100 dark:bg-gray-800 h-24 rounded-lg width-me border-2 border-gray-200 border-gray-300 focus:border-2 focus:outline-none focus:border-blue-300"
       >
       </textarea>
-
+      <div v-show="error" class="ml-2 text-red-500 font-bold trasition easy-in-out duration-500">Mumble can't be Empty</div>
       <button class="btn-primary" @click="Mumble"><fa icon="code" /> Mumble Now</button>
     </div>
     <div v-for="post in posts" :key="post.id">
@@ -24,17 +24,24 @@ export default {
     return {
       posts: [],
       content:'',
+      error:false,
     }
   },
   async fetch() {
+    this.error = false
     this.posts = await this.$axios
       .get('https://mumbleapi.herokuapp.com/api/mumbles/')
       .then((res) => res.data.results)
   },
   methods: {
     Mumble(){
-      this.$axios.post('/api/mumbles/create/',{content:this.content})
+      if (this.content.length > 0){
+        this.$axios.post('/api/mumbles/create/',{content:this.content})
       .then((res) => this.$nuxt.refresh())
+      }
+      else{
+        this.error = true;
+      }
     }
   }
 }
