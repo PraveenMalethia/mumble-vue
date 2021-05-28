bac60ef3efa456edb6b5ead3869e72d27207fcf9
ParveenMalethia <prince.malethia.36@gmail.com>
2021-05-28 13:58:09 +0530

    🦶 Footer is Now Component

---
+++ components/footer.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="bg-gray-100 dark:text-gray-200 dark:bg-gray-800">
+    <div
+      class="dark:text-gray-200 dark:bg-gray-800 flex pb-5 px-3 m-auto pt-5 text-gray-800 text-sm flex-col md:flex-row max-w-6xl"
+    >
+      <div class="mt-2">© Mumble Community 2021. <a href="https://github.com/divanov11/mumbleapi" target="_blank">Git</a></div>
+      <div class="md:flex-auto md:flex-row-reverse mt-2 flex-row flex dark:text-gray-200 dark:bg-gray-800">
+        <a href="/" class="w-6 mx-1">
+          <fa class="dark:text-gray-200"/>
+        </a>
+        <a href="/" class=" w-6 mx-1">
+          
+        </a>
+        <a href="/" class="w-6 mx-1">
+          
+        </a>
+        <a href="/" class="w-6 mx-1">
+          
+        </a>
+        <a href="/" class="w-6 mx-1">
+          
+        </a>
+        <div class="justify-ends dark:text-gray-200 dark:bg-gray-800">
+          An open source project. For the community, by the community
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Footer'
+}
+</script>
+
+<style>
+</style>
