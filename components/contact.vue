76d7b6c0be301157b96704b98477c67e2671b09d
ParveenMalethia <prince.malethia.36@gmail.com>
2021-06-01 17:22:19 +0530

    Linting fix

--- components/contact.vue
+++ components/contact.vue
@@ -3,12 +3,10 @@
     class="mr-2 flex max-w-full py-2 cursor-pointer mb-2" @click="changeCurrent(contact.username)">
     <div class="flex justify-center	md:justify-start max-w-full">
       <img src="https://mumbleapi.herokuapp.com/static/images/default.png" class="h-11 ml-5 md:ml-0 w-10">
-
       <div class="ml-1 max-w-min truncate md:pr-7 hidden md:block">
         <div class="flex justify-between md:max-w-xs mb-1">
           <p class="text-gray-800 dark:text-gray-200 -mt-1 max-w-full truncatecursor-pointer">{{ contact.username }}</p>
           <p class="text-gray-800 dark:text-gray-200 text-xs ml-2 cursor-pointer">4.14 pm</p>
-
         </div>
         <p class="text-sm text-gray-800 dark:text-gray-200 max-w-full truncate cursor-pointer">{{ contact.lastmessage }}</p>
       </div>
