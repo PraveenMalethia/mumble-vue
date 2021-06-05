606491201d7a4d4177443fe5a833263377b073b9
ParveenMalethia <prince.malethia.36@gmail.com>
2021-06-05 21:01:37 +0530

    404 Error Page FIx with error button added

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,35 +1,46 @@
 <template>
   <div
-    class="min-w-screen min-h-screen bg-blue-100 flex items-center p-5 lg:p-20 overflow-hidden relative"
+    class="
+      min-w-screen min-h-screen
+      dark:bg-gray-900
+      bg-blue-100
+      flex
+      items-center
+      p-5
+      lg:p-20
+      overflow-hidden
+      relative
+    "
   >
     <div
-      class="flex-1 min-h-full min-w-full rounded-3xl bg-white shadow-xl p-10 lg:p-20 text-gray-800 relative md:flex items-center text-center md:text-left"
+      class="
+        flex
+        min-h-full min-w-full
+        rounded-3xl
+        dark:bg-gray-600
+        dark:text-gray-300
+        bg-white
+        shadow-xl
+        p-10
+        lg:p-20
+        text-gray-800
+        relative
+        md:flex
+        items-center
+        text-center
+      "
     >
-      <div class="w-full md:w-1/2">
-        <div class="mb-10 md:mb-20 text-gray-600 font-light">
-          <h1
-            class="font-black uppercase text-3xl lg:text-5xl text-indigo-700 mb-10"
-          >
-            Error<br />404
-          </h1>
-          <p class="">Page not found</p>
-        </div>
-        <div class="mb-20 md:mb-0">
-          <button
-            @click="$router.push('/')"
-            class="text-lg font-light outline-none focus:outline-none transform transition-all hover:scale-110 text-blue-500 hover:text-blue-600"
-          >
-            Back to Home
-          </button>
-        </div>
+      <div class="flex mx-auto">
+        <img class="align-center" src="/images/login/404.svg" />
       </div>
+    <button class="btn-error" @click="$router.push('/')"> <fa icon="chevron-left" class="mr-2"/> Back to Home</button>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  layout:null
+  layout: 'auth',
 }
 </script>
 
