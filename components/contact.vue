dbc54c9ce96dea156e9a2790eae5bf1f6579c3ea
srikarkandula <79042534+srikarkandula@users.noreply.github.com>
2021-06-14 11:38:59 +0530

    Update contact.vue

--- components/contact.vue
+++ components/contact.vue
@@ -3,10 +3,14 @@
     class="mr-2 ml-3 flex max-w-full py-2 cursor-pointer mb-2" @click="changeCurrent(contact.username)">
     <div class="flex justify-center	md:justify-start max-w-full">
       <img :src="contact.created_by.profile_pic" class="h-11 ml-5 md:ml-0 w-10 rounded-full">
-      <div class="ml-1 max-w-min truncate md:pr-7 hidden md:block">
-        <div class="flex justify-between md:max-w-xs mb-1">
-          <p class="text-gray-800 dark:text-gray-200 -mt-1 max-w-full truncatecursor-pointer ml-3">{{ contact.created_by.name }}</p>
-          <p class="text-gray-800 dark:text-gray-200 text-xs ml-2 cursor-pointer">{{contact.created | moment}}</p>
+      <div class="ml-1 truncate hidden md:pr-4 md:block">
+        <div class="grid grid-cols-2 mb-1">
+          <div class="w-full col-span-1">
+            <div class="text-gray-800 dark:text-gray-200 -mt-1 max-w-full truncate cursor-pointer ml-3">{{ contact.created_by.name }}</div>
+          </div>
+          <div class="w-full col-span-1">
+            <div class="text-gray-800 dark:text-gray-200 w-full text-xs ml-2 truncate cursor-pointer">{{contact.created | moment}}</div>
+          </div>
         </div>
         <p class="text-sm text-gray-800 dark:text-gray-200 max-w-full truncate ml-3 cursor-pointer">{{ contact.content }}</p>
       </div>
