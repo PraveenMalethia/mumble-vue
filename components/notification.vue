c72e85a3bad21d21057376c79eeaeb3072f461d4
srikarkandula <79042534+srikarkandula@users.noreply.github.com>
2021-06-15 16:17:37 +0530

    Update notification.vue

--- components/notification.vue
+++ components/notification.vue
@@ -1,19 +1,21 @@
 <template>
   <div>
-    <span v-if="notification.notification_type == 'mumble' " class="flex break-words hover:bg-gray-300 dark:hover:bg-gray-700
+    <div v-if="notification.notification_type == 'mumble' " class="hover:bg-gray-300 dark:hover:bg-gray-700
       rounded-md w-full px-2 py-2">
-      <img
-        :src="notification.created_by.profile_pic"
-        style="border-radius: 50%"
-        class="h-10"
-        alt="User Profile"
-      />
+      <span class="flex break-words">
+        <img
+         :src="notification.created_by.profile_pic"
+          style="border-radius: 50%"
+          class="h-10"
+          alt="User Profile"
+        />
 
-      <p class="ml-4 text-base font-medium text-blue-500">
-        A mumble mentioning {{ notification.created_by.username }} was created
-      </p>
-
-    </span>
+        <p class="ml-4 text-base font-medium text-blue-500">
+          A mumble mentioning {{ notification.created_by.username }} was created
+        </p>
+      </span>
+      <p class="ml-14 mt-1 text-xs text-gray-500 dark:text-gray-400 font-medium">About a week ago</p>
+    </div>
   </div>
 </template>
 
